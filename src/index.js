d69f738920ea650aab0b0b678ce5c65b74bd6bb1
wesleyMirand <wewesantos@hotmail.com>
2020-05-22 17:10:16 -0300

    Segundo Commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+import App from './App';
+
+// JSX: HTMKL dentro do JavaScript (Javascript XML)
+
+render(<App />, document.getElementById('app'));
